9c1a48d2719cd6e58e92798e0fa1db4933992356
Sans <sanstwy777@outlook.com>
2020-11-03 08:04:08 +0800

    seek bar

---
+++ seekbar/src/main/java/com/sanstwy777/seekbar/MainActivity.java
@@ -0,0 +1,36 @@
+package com.sanstwy777.seekbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+
+public class MainActivity extends AppCompatActivity {
+    private ImageView image;
+    private SeekBar seekBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        image = (ImageView) findViewById(R.id.image);
+        seekBar = (SeekBar) findViewById(R.id.seekbar);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar arg0, int progress, boolean fromUser) {
+                // transparent
+                image.setImageAlpha(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar bar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar bar) {
+            }
+        });
+    }
+}
