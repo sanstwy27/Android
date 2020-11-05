0ad07febfd57e2df205b6101746ab1ed69abe228
Sans <sanstwy777@outlook.com>
2020-11-05 23:42:07 +0800

    OnLongPress

---
+++ onlongpress/src/main/java/com/sanstwy777/onlongclick/MainActivity.java
@@ -0,0 +1,28 @@
+package com.sanstwy777.onlongclick;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn = (Button) findViewById(R.id.btnShow);
+        registerForContextMenu(btn);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        menu.add("Book");
+        menu.add("Report");
+    }
+}
