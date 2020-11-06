8cc228ba7d8aa66194e22ee44f551fa753c2d5b9
Sans <sanstwy777@outlook.com>
2020-11-06 17:52:46 +0800

    OptionsMenu

---
+++ optionsmenu/src/main/java/com/sanstwy777/optionsmenu/MainActivity.java
@@ -0,0 +1,41 @@
+package com.sanstwy777.optionsmenu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = new MenuInflater(this);
+        // load menu layout
+        menuInflater.inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.settings:
+                Intent intent = new Intent(MainActivity.this, Settings.class);
+                startActivity(intent);
+                break;
+            case R.id.regard:
+                Intent intent1 = new Intent(MainActivity.this, Regard.class);
+                startActivity(intent1);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
